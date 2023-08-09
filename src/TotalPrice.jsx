a691b60629f0459fac3a352ebef8dc94350e58b7
SaBiT495 <shahedbillah495@gmail.com>
2023-08-09 13:53:54 +0600

    single added file

--- src/TotalPrice.jsx
+++ src/TotalPrice.jsx
@@ -1,13 +1,8 @@
 import { TodoItem } from "./TodoItem"
 
 export function TotalPrice({ todos }) {
-  console.log(todos.map(todo => todo.price));
-  // let sum = 0;
-  /* let sum = array.reduce(
-    (ac, cu) => ac + cu, 0
-  ); */
 
-  const totalPrice = todos.map(todo => todo.price).reduce((a, b) => a + b, 0)
+  const totalPrice = todos.map(todo => todo.price).reduce((a, b) => a + b, 0);
   return (
     <>
       <div className="flex justify-between text-lg font-bold mt-2 pt-2 border-t">
@@ -16,5 +11,4 @@ export function TotalPrice({ todos }) {
       </div>
     </>
   )
-
 }
